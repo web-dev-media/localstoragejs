db01a5aa7e5a2428d9db8f54788353789e127014
Rene Reimann <rene@die-pixler.de>
2019-07-30 07:27:26 +0200

    write tests

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,3 +1,67 @@
-const localstorage = {};
+const localstorage = {
+	options: {
+		cacheTime: 24 * 60 * 60,
+		transientKey: '',
+		cacheKey: ''
+	},
+
+	transient: function(){
+		let self = this;
+
+		let functions =  {
+			updateKey: function () {
+				if(!self.options.transientKey) {
+					self.options.transientKey = self.options.cacheKey + '_transient';
+				}
+			},
+
+			getKey: function () {
+				return self.options.transientKey;
+			},
+
+			get: function () {
+				let t = localStorage.getItem(self.options.transientKey);
+				return t ? t : this.set();
+			},
+
+			set: function () {
+				return localStorage.setItem(self.options.transientKey, (new Date().getTime() + self.options.cacheTime));
+			},
+
+			purge: function () {
+				localStorage.removeItem(self.options.transientKey);
+			}
+		};
+
+		functions.updateKey();
+
+		return functions;
+	},
+
+	shouldUpdateStorage: function(localStorageKey) {
+		if ( !this.get(localStorageKey) ) {
+			return true;
+		}
+
+		return this.transient().get() ? this.transient().get() < new Date().getTime() : true;
+	},
+
+	update: function(localStorageKey, data){
+		localStorage.setItem( this.transient().getKey(), (new Date().getTime() + this.options.cacheTime ));
+		localStorage.setItem( localStorageKey, JSON.stringify(data));
+	},
+
+	get: function(localStorageKey){
+		this.options.cacheKey = localStorageKey ? localStorageKey : false;
+		this.sorageData = localStorage.getItem( this.options.cacheKey );
+
+		return this.sorageData ? JSON.parse(this.sorageData) : null;
+	},
+
+	purge: function(localStorageKey) {
+		localStorage.removeItem(localStorageKey);
+		this.transient().purge();
+	}
+};
 
 module.exports = localstorage;
--- tests/localstorage.test.js
+++ tests/localstorage.test.js
@@ -1,4 +1,4 @@
-const localsorage = require('../localstorage.js');
+const localsorageHandle = require('../localstorage.js');
 
 const spyError = jest.spyOn( console, 'error' );
 const spyWarn = jest.spyOn( console, 'warn' );
@@ -8,6 +8,32 @@ beforeEach( () => {
   spyWarn.mockReset();
 } );
 
-test('a test', () => {
-  const testObj = 42;
+const KEY = "foo",
+			TIME = 10,
+			DATA = "bar";
+
+test('should set cacheKey', () => {
+	localsorageHandle.options.cacheKey = KEY;
+	expect(localsorageHandle.options.cacheKey).toBe(KEY);
+});
+
+test('should set cacheTime', () => {
+	localsorageHandle.options.cacheTime = TIME;
+	expect(localsorageHandle.options.cacheTime).toBe(10);
+});
+
+
+test('should update localStorage', () => {
+ 	localsorageHandle.update(KEY, DATA);
+
+	expect(localsorageHandle.options.transientKey).toBe(KEY + '_transient');
+	expect(localsorageHandle.get(KEY)).toBe(DATA);
+});
+
+test('shouldUpdateStorage() do not update', () => {
+	expect(localsorageHandle.shouldUpdateStorage(KEY)).toBe(false);
+});
+
+test('should save to localStorage', () => {
+	console.log(localStorage.__STORE__);
 });
