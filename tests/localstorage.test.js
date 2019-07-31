7cb6c1a64c3cf5d3d53c159279770a80549528eb
Rene Reimann <rene@die-pixler.de>
2019-07-31 06:26:41 +0200

    write tests

--- tests/localstorage.test.js
+++ tests/localstorage.test.js
@@ -6,6 +6,8 @@ const spyWarn = jest.spyOn( console, 'warn' );
 beforeEach( () => {
   spyError.mockReset();
   spyWarn.mockReset();
+
+	localStorage.__STORE__.clear();
 } );
 
 const KEY = "foo",
@@ -23,17 +25,31 @@ test('should set cacheTime', () => {
 });
 
 
-test('should update localStorage', () => {
+test('update localStorage', () => {
  	localsorageHandle.update(KEY, DATA);
 
 	expect(localsorageHandle.options.transientKey).toBe(KEY + '_transient');
 	expect(localsorageHandle.get(KEY)).toBe(DATA);
 });
 
+test('shouldUpdateStorage() do update', () => {
+	expect(localsorageHandle.shouldUpdateStorage(KEY)).toBe(true);
+});
+
 test('shouldUpdateStorage() do not update', () => {
+	localsorageHandle.update(KEY, DATA);
 	expect(localsorageHandle.shouldUpdateStorage(KEY)).toBe(false);
 });
 
-test('should save to localStorage', () => {
-	console.log(localStorage.__STORE__);
+test('shouldUpdateStorage() transient timout after 10s', () => {
+	let then = new Date().getTime() + TIME;
+	let loop = [0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7,];
+
+	localsorageHandle.update(KEY, DATA);
+	expect(localsorageHandle.shouldUpdateStorage(KEY)).toBe(false);
+	// wait
+	for(let i = 0; i < loop.length; i++){	}
+	expect(localsorageHandle.shouldUpdateStorage(KEY)).toBe(true);
+
+
 });
