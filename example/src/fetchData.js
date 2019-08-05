7f8fa85614c615d9290ec208321c04e6383d8908
Rene Reimann <rene@die-pixler.de>
2019-08-05 09:05:09 +0200

    add mediaplayer example

---
+++ example/src/fetchData.js
@@ -0,0 +1,77 @@
+var lsHandle = require('../../src/localstorage.js');
+
+// override the default cacheTime
+lsHandle.options.cacheTime = 5 * 60 * 60;
+
+// define a cacheKeyPrefix
+var cacheKeyPrefix = 'lsHandle_example_';
+
+// start a call for some dummy data
+var getData = async () => {
+	var data = [];
+	var endpoints = ['photos', 'posts', 'comments'];
+	var fromLocalstorage = false;
+
+	for(var i = 0; i < endpoints.length; i++) {
+		var endpoint = endpoints[i];
+		var cacheKey = cacheKeyPrefix + endpoint;
+
+		// at first ask the cache if there some data in the cache
+		if (lsHandle.shouldUpdateStorage(cacheKey)) {
+			data[endpoint] = await fetchData(endpoint);
+		}else{
+
+			// if there some Data in the cache load it from the localstorage
+			fromLocalstorage = true;
+			data[endpoint] = lsHandle.get(cacheKey);
+		}
+	}
+
+	data['loadedFromCache'] = fromLocalstorage;
+	data['options'] = lsHandle.options;
+
+	return data;
+};
+
+// go fetch the data from the internet
+var fetchData = async (endPoint) => {
+	return new Promise(function (resolve, reject) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', 'https://jsonplaceholder.typicode.com/' + endPoint);
+		xhr.onload = function () {
+			if (this.status >= 200 && this.status < 300) {
+				let data = JSON.parse(xhr.response);
+				storeData(data, endPoint);
+				resolve(data);
+			}
+		};
+		xhr.send();
+	});
+};
+
+// after fetchData() write this into the localstorage
+var storeData = (data, endPoint) => {
+	if(data && endPoint){
+		lsHandle.update(cacheKeyPrefix + endPoint, data);
+	}
+};
+
+/* run example on document loaded */
+document.addEventListener('DOMContentLoaded', function(event) {
+	var zeit0 = performance.now();
+	new Promise((resolve, reject) => {
+			var data = getData();
+
+			if(data){
+				resolve(data);
+			}
+		}).then( (data) => {
+		console.log(data);
+		var zeit1 = performance.now();
+		console.log("Der Aufruf von getData dauerte " + (zeit1 - zeit0) + " ms.");
+	});
+
+});
+
+
+
---
+++ example/src/mediaplayer.js
@@ -0,0 +1,193 @@
+let lsHandle = require('../../src/localstorage.js');
+
+let mediaPlayer = () => {
+	let self = this;
+	return {
+		options: {
+			storage_key: 'audio-play-storage',
+			currentpos : 0,
+			selectors  : {
+				player        : '#media-audio',
+				playerControls: {
+					startStop  : '#play-pause-button',
+					mute       : '#mute-button',
+					progressBar: '#progress-bar'
+				}
+			}
+		},
+	/*
+		events: {
+			//play: storage.set( 'mediaPlayer.isPlaying', 1 ),
+			//stop: storage.set( 'mediaPlayer.isPlaying', 0 ),
+		},
+
+		selectNodes: {
+			if ( this.selectors ) {
+				for ( selector in this.selectors ) {
+
+					//document.querySelector('#media-audio' )
+
+				}
+			}
+		},
+
+		addEvents: {
+			if ( this.events ) {
+				for ( event in this.events ) {
+					/!*	mediaPlayer.addEventListener( 'play', function() {
+							storage.set( 'mediaPlayer.isPlaying', 1 );
+						}, false );*!/
+				}
+			}
+		},
+	*/
+		init: () => {
+			console.log( self.options );
+		}
+	}
+};
+
+document.addEventListener('DOMContentLoaded', function(event) {
+	console.log(mediaPlayer().init());
+});
+
+/*
+var mediaPlayer;
+var storage;
+var storage_key = 'audio-play-storage';
+var currentpos = 0;
+var progressBar;
+
+function initialiseMediaPlayer() {
+
+	//get the mediaplayer element
+	mediaPlayer = document.getElementById( 'media-audio' );
+
+	//update currentpos in at the stoarage
+	mediaPlayer.addEventListener('timeupdate', Progress, false );
+
+	//reset the player
+	mediaPlayer.addEventListener('ended', Reset, false );
+
+	//inital the storage
+	storage = new Persist.Store( storage_key );
+
+	currentpos = storage.get( storage_key );
+
+	if( currentpos > 0 ){
+		setPlayPos( currentpos );
+	}
+
+	//select the play Button
+	playPauseBtn = document.getElementById('play-pause-button');
+
+	muteBtn = document.getElementById('mute-button');
+
+	progressBar = document.getElementById('progress-bar');
+
+	//Supports the default player ui
+	mediaPlayer.addEventListener('play', function(){
+		storage.set( 'mediaPlayer.isPlaying', 1 );
+	}, false );
+
+	mediaPlayer.addEventListener('pause', function(){
+		storage.set( 'mediaPlayer.isPlaying', 0 );
+	}, false );
+
+
+}
+
+function setPlayPos( ToSetPos ) {
+
+	mediaPlayer_sources = mediaPlayer.getElementsByTagName( 'source' );
+
+	for( i = 0; i < mediaPlayer_sources.length; ++i ) {
+
+		src = mediaPlayer_sources[i].getAttribute("src").split('#');
+		mediaPlayer_sources[i].setAttribute("src", src[0] + '#t=' + ToSetPos );
+		mediaPlayer.currentTime = ToSetPos;
+
+	}
+
+	if( storage.get( 'mediaPlayer.isPlaying' ) == 1 ){
+		mediaPlayer.play();
+	}
+
+}
+
+function Progress() {
+	//cache the current pos.
+	storage.set( storage_key, mediaPlayer.currentTime);
+
+	// Work out how much of the media has played via the duration and currentTime parameters
+	var percentage = Math.floor( ( 100 / mediaPlayer.duration ) * mediaPlayer.currentTime );
+
+	// Update the progress bar's value
+	progressBar.value = percentage;
+	// Update the progress bar's text (for browsers that don't support the progress element)
+	progressBar.innerHTML = percentage + '% played';
+
+}
+
+function Reset() {
+	storage.set( storage_key, '' );
+}
+
+function togglePlayPause() {
+
+	setPlayPos( mediaPlayer.currentTime );
+
+	// If the mediaPlayer is currently paused or has ended
+	if ( mediaPlayer.paused || mediaPlayer.ended ) {
+		// Change the button to be a pause button
+		changeButtonType( playPauseBtn, 'pause' );
+		// Play the media
+		mediaPlayer.play();
+
+
+		storage.set( 'mediaPlayer.isPlaying', 1 );
+	}
+	// Otherwise it must currently be playing
+	else {
+		// Change the button to be a play button
+		changeButtonType( playPauseBtn, 'play' );
+		// Pause the media
+		mediaPlayer.pause();
+
+		storage.set( 'mediaPlayer.isPlaying', 0 );
+	}
+}
+
+// Stop the current media from playing, and return it to the start position
+function replayMedia() {
+	mediaPlayer.pause();
+	mediaPlayer.currentTime = 0;
+	mediaPlayer.play();
+}
+
+// Updates a button's title, innerHTML and CSS class to a certain value
+function changeButtonType(btn, value) {
+	btn.title = value;
+	btn.innerHTML = value;
+	btn.className = value;
+}
+
+function togglePos(){
+
+	progressBar.addEventListener('click', function (event) {
+
+		elmwith = progressBar.offsetWidth;
+
+		var xconvert = event.offsetX / elmwith;
+		var finalx = (xconvert).toFixed(1);
+
+		time = mediaPlayer.duration * finalx;
+
+		setPlayPos( time );
+
+
+	});
+
+}*/
+
+
